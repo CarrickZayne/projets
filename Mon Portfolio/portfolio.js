74b97c345a509ebc92cd16a5ea885e58ccddc35d
CarrickZayne <66408026+CarrickZayne@users.noreply.github.com>
2020-08-14 15:06:33 +0200

    Add files via upload

---
+++ Mon Portfolio/portfolio.js
@@ -0,0 +1,4 @@
+const welcome_section = document.getElementById("welcome-section");
+const project_secion = document.getElementById("projects");
+const navbar = document.getElementById("navbar");
+const profile_link = document.getElementById("profile-link");
